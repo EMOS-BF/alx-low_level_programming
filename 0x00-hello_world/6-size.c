3a375e67013965962afd7c357e0c0f10485114b9
EMOS-BF <sourbar2001@gmail.com>
2022-06-06 03:46:41 -0700

    task 4

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void){
+  int a;
+  long int b;
+  long long int c;
+  char d;
+  float f;
+  printf("size of a char %lu bytes\n" (unsigned long)sizeof(d));
+  printf("size of an int %lu bytes\n" (unsigned long)sizeof(a));
+  printf("size of a long int %lu byte\n" (unsigned long)sizeof(b));
+  printf("size of a long long int %lu bytes\n" (unsigned long)sizeof(c));
+  printf("size of a float %lu bytes\n" (unsigned long)sizeof(f));
+  return (0);
+}
