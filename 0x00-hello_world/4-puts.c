6e7a57cd74b924d6c61a4f31b85ef89f57a79a5f
EMOS-BF <sourbar2001@gmail.com>
2022-06-06 04:07:59 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /*
-main function
-  return : 0
+ *main function
+ *return : 0
 */
 
 int main(void)
@@ -9,5 +9,5 @@ int main(void)
   
 puts("\"Programming is like building a multilingual puzzle");
   
-return 0;
+ return (0);
 }
