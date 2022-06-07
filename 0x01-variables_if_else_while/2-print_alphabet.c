3c319b99bd16d9724023e93a050c6b3535ed600b
EMOS-BF <sourbar2001@gmail.com>
2022-06-06 17:13:38 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+*main -prints the alphabet in lowercase
+*followed by a new line
+*return : 0
+*/
+int main(void)
+{
+  char ch;
+  for(ch ='a'; ch <='2';ch++)
+    {
+      putchar(ch);
+    }
+  putchar('\n');
+  return (0);
+}
