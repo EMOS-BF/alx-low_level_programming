86af9b87a27e39f061c6f7ba9cfdc0cbb6f6d037
SOME <sourbar2001@gmail.com>
2022-09-14 07:56:06 -0700

    commit for task 9

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,7 +4,7 @@
 *
 * h = hour, m = minutes
 *
-* / 10 allows second digit to rotate
+*  10 allows second digit to rotate
 *
 * for loop breaks before passing 24:00
 *
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
 * times_table -  a function that prints the 9 times table, starting with 0
 *
@@ -16,12 +16,24 @@ for (rone = 0; rone <= 9; rone++)
 _putchar('0');
 _putchar(',');
 _putchar(' ');
-for (cone = 1; cone <= 9; cone++)												{
+for (cone = 1; cone <= 9; cone++)
+{
 d = (rone * cone);
-if ((d / 10) > 0)														{																_putchar((d / 10) + '0');																}
-else																{															
-_putchar(' ');		}																													_putchar((d % 10) + '0');													if (cone < 9)
-{																_putchar(',');															_putchar(' ');																}															}
+if ((d / 10) > 0)
+{
+_putchar((d / 10) + '0');
+}
+else
+{
+_putchar(' ');
+}
+_putchar((d % 10) + '0');
+if (cone < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
 _putchar('\n');
 }
 }
