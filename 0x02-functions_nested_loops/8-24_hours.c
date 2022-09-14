1eb727d3e5a9d3efc9aacb25f2948d7b4f37cc94
SOME <sourbar2001@gmail.com>
2022-09-14 07:13:47 -0700

    commit for task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
 * jack_bauer - function that prints every minute of the day, 24 hour clock
 *
@@ -17,7 +17,7 @@ for (h = 0; h < 24; h++)
 {
 for (m = 0; m < 60; m++)
 {
-_putchar((h / 10) + '0');																_putchar((h % 10) + '0');
+_putchar((h / 10) + '0');													_putchar((h % 10) + '0');
 _putchar(':');					
 _putchar((m / 10) + '0')								
 _putchar((m % 10) + '0');
