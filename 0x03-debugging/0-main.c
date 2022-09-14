589850ece2287787cf27aee0038f4d0dcbd829b9
SOME <sourbar2001@gmail.com>
2022-09-14 10:18:39 -0700

    O-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,31 @@
-#include "main.h"
-/**
-* main - takes a date and prints how many days are left in the year, taking
-* leap years into account
-* Return: 0
-*/
+#include "main.h"                                                                                                               
 
-int main(void)
-{
-int i;
-i=0;
+/**                                                                                                                             
 
-positive_or_negative(i);
+* main - takes a date and prints how many days are left in the year, taking                                                     
 
-return (0);
-}
+* leap years into account                                                                                                       
+
+* Return: 0                                                                                                                     
+
+*/                                                                                                                              
+
+                                                                                                                                
+
+int main(void)                                                                                                                  
+
+{                                                                                                                               
+
+int i;                                                                                                                          
+
+i=0;                                                                                                                            
+
+                                                                                                                                
+
+positive_or_negative(i);                                                                                                        
+
+                                                                                                                                
+
+return (0);                                                                                                                     
+
+}                  
