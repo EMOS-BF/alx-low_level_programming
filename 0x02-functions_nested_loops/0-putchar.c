564c337e18f3a3c9a61da733f8cfb1ff2e8c5371
SOME <sourbar2001@gmail.com>
2022-09-14 05:03:04 -0700

    commit for 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**                                              
-*main -print ptchar 
-*                                                                               * Return:always 0                                              
-*/                                                                              int main(void)                                              
+*main -print ptchar                                                             * Return:always 0
+*/
+int main(void)                                              
 {                                                                               _putchar('_');                                               
 _putchar('p');                                                                  _putchar('u');                                              
 _putchar('t');                                                                  _putchar('c');                                              
