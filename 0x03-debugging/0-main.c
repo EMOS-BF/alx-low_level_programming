f3dbd4ee8364e040fb454a7c9c53b65bbb4eabfd
SOME <sourbar2001@gmail.com>
2022-09-14 09:09:23 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - takes a date and prints how many days are left in the year, taking
+* leap years into account
+* Return: 0
+*/
+int main(void)
+{
+int i;
+i=0;
+positive_or_negative(i);
+return (0);
+}
