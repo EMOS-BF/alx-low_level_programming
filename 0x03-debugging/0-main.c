82844e6c433d25643b82befc20b5cd19b5265199
SOME <sourbar2001@gmail.com>
2022-09-15 14:53:13 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,12 @@
-#include "main.h"                                                                                                               
-
-/**                                                                                                                             
-
-* main - takes a date and prints how many days are left in the year, taking                                                     
-
-* leap years into account                                                                                                       
-
-* Return: 0                                                                                                                     
-
-*/                                                                                                                              
+#include "main.h"
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 int main(void)
-{                                                                                                                               
+{
 int i;
-i=0;
+i = 0;
 positive_or_negative(i);
 return (0);
-}                  
+}
