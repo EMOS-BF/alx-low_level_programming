58710f1b5249110c27b26aedf336386ca9c1c14c
SOME <sourbar2001@gmail.com>
2022-09-19 17:40:07 -0700

    task4

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,12 +13,12 @@ len1=0;
 len2 = 0;
 while(s[len1] != '\0')
 	len1++;
-len2=len1-1;
-for (a = 0;a<(len1/2; a++)
+len2 = len1 - 1;
+for (a = 0 ; a<(len1/2) ; a++)
 {
 tmp=s[a];
 s[a]=s[len2];
 s[len2]=tmp;
-len2 -= 1;
+len2 - = 1;
 }
 }
