369f2ac9911d33b2d4be8776a8706ffa06b452d1
SOME <sourbar2001@gmail.com>
2022-09-21 09:38:46 -0700

    task6

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,10 +12,12 @@ int _strcmp(char *s1, char *s2)
 {
 int i;
 for(i = 0; s1[i] == s2[i] && s1[i] == '\0'; i++);
-if(s1[i] != s2[i]){
+if(s1[i] != s2[i])
+{
 return (*s1 - *s2);
 }
-else{
+else
+{
 return 0;
 }
 }
