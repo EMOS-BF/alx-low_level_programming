5dade7c3f2805f8168aea4171c8f3bc68df437ce
SOME <sourbar2001@gmail.com>
2022-09-21 09:23:47 -0700

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,19 +1,39 @@
 #include "main.h"
 /**
 * cap_string - Write a function that capitalizes all words of a string.
-*
 * @entry: This is the input string
 *
 * Return: String capitalized
 */
 char *cap_string(char *entry)
 {
-while (entry[i] != '\0')
+#include "holberton.h"
+/**
+* cap_string - Write a function that capitalizes all words of a string
+* @entry: This is the input string
+* Return: String capitalized
+*/
+char *cap_string(char *entry)
 {
-if (entry[i]  >= 97 &&  entry[i] <= 122)
+int conversion, index, count;
+char chars[]={' ', ',', ';', '.', '!','?', '"', '(',')', '{', '}',  '\t', '\n, '\0'};
+conversion = 32;
+for (index = 0; entry[index] != '\0'; index++)
 {
-entry[i] = entry[i] - 32; 
-i++;
+if (entry[index] >= 'index' && entry[index] <= 'z')
+{
+entry[index] =  entry[index] - conversion;
 }
-return entry;
+conversion = 0;
+for (count = 0; chars[count] != '\0'; count++)
+{
+if (chars[count] == entry[index])
+{
+conversion = 32;
+break;
+}
+}
+}
+return (entry);
+}
 }
