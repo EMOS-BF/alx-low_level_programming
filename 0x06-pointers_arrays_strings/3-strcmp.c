98d2121d8979847b5cc9246b834ba72162d0f14f
SOME <sourbar2001@gmail.com>
2022-09-22 13:14:18 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,12 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-int counter, cmpVal;
+int count, Val;
 counter = 0;
-while (s1[counter] == s2[counter] && s1[counter] != '\0')
+while (s1[count] == s2[count] && s1[count] != '\0')
 {
-counter++;
+count++;
 }
-cmpVal = s1[counter] - s2[counter];
-return (cmpVal);
+Val = s1[count] - s2[count];
+return (Val);
 }
