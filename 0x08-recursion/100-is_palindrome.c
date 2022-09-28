aa59e461c61c221a2dd36f31d6adb524682adb19
SOME <sourbar2001@gmail.com>
2022-09-28 07:27:04 -0700

    task 7 commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,7 +4,7 @@
 * _print_rev_recursion - Write a function that prints a string in reverse.
 * @s: This is my entry
 */
-char _print_rev_recursion(char *s)
+char *_print_rev_recursion(char *s)
 {
 if (*s != '\0')
 {
