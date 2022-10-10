4a6952b53e3e637e6648d5b2024501fa3d781d6d
SOME <sourbar2001@gmail.com>
2022-10-10 03:03:51 -0700

    README.md and task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* struct dog - this descripction about which dog
+* @name: First member of the struct
+* @age:  second member of the struct
+* @owner:  third member of the struct
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
