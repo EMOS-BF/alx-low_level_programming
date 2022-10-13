1495e1dba52cde0deb3ae77c7e10139b54b174bf
SOME <sourbar2001@gmail.com>
2022-10-13 11:31:42 -0700

    commit task2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,24 +9,22 @@
 * @n: This is the number of items
 *
 */
-
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list ap;
-	unsigned int count;
-
-	va_start(ap, n);
-	for (count = 0; count < n; count++)
-	{
-		char *s;
-		s = va_arg(ap, char *);
-		if (s == NULL)
-			printf("(nil)");
-		else
-			printf("%s", s);
-		if (separator != NULL && (count + 1) != n)
-			printf("%s", separator);
-	}
-	printf("\n");
-	va_end(ap);
+va_list ap;
+unsigned int count;
+va_start(ap, n);
+for (count = 0; count < n; count++)
+{
+char *s;
+s = va_arg(ap, char *);
+if (s == NULL)
+	printf("(nil)");
+else
+	printf("%s", s);
+if (separator != NULL && (count + 1) != n)
+	printf("%s", separator);
+}
+printf("\n");
+va_end(ap);
 }
