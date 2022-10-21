17d001d1ce4cd825a9db628ec209459751f38443
SOME <sourbar2001@gmail.com>
2022-10-21 11:53:35 -0700

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,18 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "lists.h"
+#include <stdlib.h>                                                                                                        
+#include <string.h>                                                                                                        
+#include "lists.h"                                                                                                         
+/**                                                                                                                        
+* add_node - Add new node to a linked list.                                                                                
+* @head: head of the linked list.                                                                                          
+* @str: string to add.                                                                                                     
+* Return: pointer of the new element added to linked list.                                                                 
+*/ 
+list_t *add_node(list_t **head, const char *str)                                                                           
+{                                                                                                                          
+list_t *newnode;                                                                                                           
+newnode = (list_t *)malloc(sizeof(list_t));                                                                                
+if (newnode == NULL)                                                                                                       
+{                                                                                                                          
+    return (NULL);                                                                                                         
+}                                                                                                                          
+newnode->str = strdup(str);                                                                                                
+newnode->len = _strlen(str);                                                                                               
+newnode->next = *head;                                                                                                     
+*head = newnode;                                                                                                           
+return (newnode);                                                                                                          
+}
 /**
-* add_node - Add new node to a linked list.
-* @head: head of the linked list.
-* @str: string to add.
-* Return: pointer of the new element added to linked list.
-*/
-
-/*
-*_strlen-calculate string lenght
-*s:a string parameter of _strlen
-*return: the lenght of the string
+* _strlen - Calculate lenght of a string.
+* @s: string.
+* Return: lenght of the string s.
 */
 
 int _strlen(const char *s)
@@ -21,19 +33,4 @@ int idx = 0;
 for (; s[idx]; idx++)
 ;
 return (idx);
-}
-
-list_t *add_node(list_t **head, const char *str)
-{
-list_t *newnode;
-newnode = (list_t *)malloc(sizeof(list_t));
-if (newnode == NULL)
-{
-    return (NULL);
-}
-newnode->str = strdup(str);
-newnode->len = _strlen(str);
-newnode->next = *head;
-*head = newnode;
-return (newnode);
 }
