5f2e4f3fa895216ff0cecbe01b477aca0da743f2
SOME <sourbar2001@gmail.com>
2022-10-25 06:41:56 -0700

    task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,23 +3,22 @@
 /**
 * pop_listint-deletes the head node of a listint_t
 *@head: Adress of the first node
-* Return: Integer if success.
 */
 int pop_listint(listint_t **head)
 {
-    listint_t *current = *head;
-    listint_t *previous = *head;
-    int temp;
-    if (*head == NULL)
-    {
-        return (0);
-    }
-    else
-    {
-        *head =current->next;
-        temp = current->n;
-        free(current);
-        current = NULL;
-        return (temp);
-    }
+listint_t *current = *head;
+listint_t *previous = *head;
+int temp;
+if (*head == NULL)
+{
+return (0);
+}
+else
+{
+*head = current->next;
+temp = current->n;
+free(current);
+current = NULL;
+return (temp);
+}
 }
