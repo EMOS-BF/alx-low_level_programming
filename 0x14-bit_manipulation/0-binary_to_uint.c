37eed8ec50ce5a9a569cf1ff23179b9623620801
SOME <sourbar2001@gmail.com>
2022-10-27 10:09:31 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,16 +7,31 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-    int slen = strlen(b);
-    int decal =1;
-    int convert_number = 0;
-    for (int i = (slen-1); i>=0; i--)
-    {
-        if (b[i] == '1')
-        {
-            convert_number += decal;
-        }
-        decal *= 2;
-    }
-    return convert_number;
+int slen = strlen(b);
+int decal = 1;
+int convert_number = 0;
+if (b == NULL)
+{
+return (0);
+}
+else
+{
+for (int i = (slen - 1); i >= 0; i--)
+{
+if (b[i] == '1')
+{
+convert_number += decal;
+}
+else if (b[i] == '0')
+{
+convert_number += 0;
+}
+else
+{
+return (0);
+}
+decal *= 2;
+}
+}
+return (convert_number);
 }
