ba8b87bdb5dcd4c70c726a78760318282f8e711d
SOME <sourbar2001@gmail.com>
2022-12-12 10:15:10 -0800

    Corection of task 1 and task 0

---
+++ 0x18-dynamic_libraries/memset.c
@@ -0,0 +1,92 @@
+#include "main.h"
+#include <stdio.h>
+int _putchar(char c)
+{
+return 0;
+}
+
+int _islower(int c)
+{
+return 0;
+}
+
+int _isalpha(int c)
+{
+return 0;
+}
+
+int _abs(int n)
+{
+return 0;
+}
+
+int _strlen(char *s){
+return 0;
+}
+
+void _puts(char *s)
+{
+printf("hello");
+}
+
+char *_strcpy(char *dest, char *src)
+{
+char *s = "SOME";
+return s;
+}
+
+int _atoi(char *s)
+{
+return 0;
+}
+
+char *_strcat(char *dest, char *src)
+{
+char *s = "SOME";
+return s;
+}
+
+char *_strncat(char *dest, char *src, int n)
+{
+return "SOME";
+}
+
+char *_strncpy(char *dest, char *src, int n)
+{
+return "SOME";
+}
+
+int _strcmp(char *s1, char *s2)
+{
+return 0;
+}
+
+char *_memset(char *s, char b, unsigned int n)
+{
+return "SOME";
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+return "SOME";
+}
+
+char *_strchr(char *s, char c)
+{
+return "SOME";
+}
+
+unsigned int _strspn(char *s, char *accept)
+{
+return 0;
+}
+
+char *_strpbrk(char *s, char *accept)
+{
+return "SOME";
+}
+
+char *_strstr(char *haystack, char *needle)
+{
+return "SOME";
+}
