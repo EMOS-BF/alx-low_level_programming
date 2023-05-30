82babdaaf7464a2e8db4b0a05bc00f79574d32c8
SOME <sourbar2001@gamail.com>
2023-05-29 21:11:15 -0700

    advance task

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,17 +1,16 @@
 #include "main.h"
-/**
-* get_endianness - Check the endianess
-*
-* Return: void
-*/
 
+/**
+* get_endianness - check endianness
+* Return: 0 if big endian, or 1 if little endian
+*/
 int get_endianness(void)
 {
-int index;
-index = 2;
-if (index & 1)
-{
-return (0);
-}
-return (1);
+	int num;
+
+	num = 1;
+	if (*(char *)&num == 1)
+		return (1);
+	else
+		return (0);
 }
