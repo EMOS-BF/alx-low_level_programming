034488bfe73f657c055330b90c0f285e4373961b
SOME <sourbar2001@gamail.com>
2023-06-04 16:03:37 -0700

    task 7 update

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,36 +1,33 @@
 #include "lists.h"
+
 /**
-* add_dnodeint - adds a new node at the beginning
-* of a dlistint_t list
-*
-* @head: head of the list
-* @n: value of the element
-* Return: the address of the new element
-*/
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
+ * @head: pointer to the headn node of the linked list.
+ * @n: data to be stored in the new node.
+ * Return: address of the new element, or NULL if it failed
+ **/
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *new;
-dlistint_t *h;
-new = malloc(sizeof(dlistint_t));
-if (new == NULL)
-{
-return (NULL);
-}
-new->n = n;
-new->prev = NULL;
-h = *head;
-if (h != NULL)
-{
-while (h->prev != NULL)
-{
-h = h->prev;
-}
-}
-new->next = h;
-if (h != NULL)
-{
-h->prev = new;
-}
-*head = new;
-return (new);
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	if (*head == NULL)
+	{
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		*head = new_node;
+	}
+	else
+	{	new_node->next = *head;
+		new_node->prev = NULL;
+		(*head)->prev = new_node;
+		*head = new_node;
+	}
+	return (new_node);
 }
--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,36 +1,38 @@
 #include "lists.h"
+
 /**
-* add_dnodeint_end - adds a new node at the end
-* of a dlistint_t list
-*
-* @head: head of the list
-* @n: value of the element
-* Return: the address of the new element
-*/
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list.
+ * @head: pointer to the head node of the linked list.
+ * @n: data to be stored in the new node.
+ * Return: address of the new element, or NULL if it failed.
+ **/
+
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-dlistint_t *h;
-dlistint_t *new;
-new = malloc(sizeof(dlistint_t));
-if (new == NULL)
-{
-return (NULL);
-}
-new->n = n;
-new->next = NULL;
-h = *head;
-if (h != NULL)
-{
-while (h->next != NULL)
-{
-h = h->next;
-}
-h->next = new;
-}
-else
-{
-*head = new;
-}
-new->prev = h;
-return (new);
+	dlistint_t *new_node, *last_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		new_node->next = NULL;
+		*head = new_node;
+	}
+	else
+	{
+		last_node = *head;
+		while (last_node->next != NULL)
+		{
+			last_node = last_node->next;
+		}
+		last_node->next = new_node;
+		new_node->prev = last_node;
+		new_node->next = NULL;
+	}
+	return (new_node);
 }
