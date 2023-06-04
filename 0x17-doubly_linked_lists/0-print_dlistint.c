61e751885e8c398cfe0ee60998dcdae0f23157ba
SOME <sourbar2001@gamail.com>
2023-06-04 15:33:32 -0700

    task 0 update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,15 +8,13 @@
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *temp;
-	size_t count;
+	int numnode = 0;
 
-	temp = h;
-	while (temp && temp->prev)
-		temp = temp->prev;
-
-	for (count = 0; temp; count++, temp = temp->next)
-		printf("%d\n", temp->n);
-
-	return (count);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		numnode++;
+	}
+	return (numnode);
 }
