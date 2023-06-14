ee10643a4f3423a8f3fa6929d4f935e930f32d95
SOME <sourbar2001@gamail.com>
2023-06-14 08:54:42 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,24 +10,32 @@
 **/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *ptr1 = *head;
-listint_t *ptr2 = (listint_t *)malloc(sizeof(listint_t));
-ptr2->n = n;
-ptr2->next = NULL;
-if (*head == NULL && idx != 0)
-{
-return (NULL);
-}
-if (ptr2 == NULL)
-{
-return (NULL);
-}
-while (idx != 1)
-{
-ptr1 = ptr1->next;
-idx--;
-}
-ptr2->next = ptr1->next;
-ptr1->next = ptr2;
-return (ptr1);
+	listint_t *new, *copy = *head;
+	unsigned int node;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = copy;
+		*head = new;
+		return (new);
+	}
+
+	for (node = 0; node < (idx - 1); node++)
+	{
+		if (copy == NULL || copy->next == NULL)
+			return (NULL);
+
+		copy = copy->next;
+	}
+
+	new->next = copy->next;
+	copy->next = new;
+
+	return (new);
 }
